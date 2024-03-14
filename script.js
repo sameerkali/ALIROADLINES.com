82e8dc7512587f24b7907d29bc6e4b56559bf7f7
Sameer Faridi <s.faridi007@gmail.com>
2024-03-14 16:33:04 +0530

    changes v1.0.0

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["transpoters"],
+        strings: ["transpoter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
