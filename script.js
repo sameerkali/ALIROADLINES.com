66645e9617e6c027fe405e1c947c23cc6e64d893
Sameer Faridi <s.faridi007@gmail.com>
2024-04-03 19:54:37 +0530

    spelling correction and company addition

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["transpoters"],
+        strings: ["transporters"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["transpoter"],
+        strings: ["transporter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
